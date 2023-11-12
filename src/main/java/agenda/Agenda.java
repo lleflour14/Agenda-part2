aa53e36c6af5ade597a655c5dd381b8635879d5e
lleflour <louise.leflour@sfr.fr>
2023-11-12 17:01:57 +0100

    fin de tp

--- src/main/java/agenda/Agenda.java
+++ src/main/java/agenda/Agenda.java
@@ -5,26 +5,74 @@ import java.util.*;
 
 /**
  * Description : An agenda that stores events
+ *
  */
 public class Agenda {
-    /**
-     * Adds an event to this agenda
-     *
-     * @param e the event to add
-     */
-    public void addEvent(Event e) {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+    private ArrayList<Event> thoseEvents;
+
+    public Agenda(){
+        this.thoseEvents = new ArrayList<>();
     }
+    public void addEvent(Event e) {
+        thoseEvents.add(e);
+
+    }
+
 
     /**
      * Computes the events that occur on a given day
      *
-     * @param day the day toi test
+     * @param day the day to i test
      * @return a list of events that occur on that day
      */
     public List<Event> eventsInDay(LocalDate day) {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        List<Event> thoseEventsDay = new ArrayList<Event>();
+        for (int i = 0; i < thoseEvents.size(); i ++){
+            if (thoseEvents.get(i).isInDay(day)){
+                thoseEventsDay.add(thoseEvents.get(i));
+            }
+        }
+        //
+        return thoseEventsDay;
     }
+
+    /**
+     * Trouver les événements de l'agenda en fonction de leur titre
+     * @param title le titre à rechercher
+     * @return les événements qui ont le même titre
+     */
+    public List<Event> findByTitle(String title) {
+        ArrayList<Event> listeTitre = new ArrayList<Event>();
+        for (Event e : thoseEvents){
+            if(e.getTitle().equals(title)){
+                listeTitre.add(e);
+            }
+        }
+        return listeTitre;
+    }
+
+    /**
+     * Déterminer s’il y a de la place dans l'agenda pour un événement
+     * @param e L'événement à tester (on se limitera aux événements simples)
+     * @return vrai s’il y a de la place dans l'agenda pour cet événement
+     */
+    public boolean isFreeFor(Event e) {
+        Boolean b =true;
+        for (Event ev : thoseEvents){
+            if (ev.getStart().isBefore(e.getStart())){
+                if (ev.getStart().plus(ev.getDuration()).isAfter(e.getStart()) ){
+                    b=false;
+                }
+            } else if (ev.getStart().equals(e.getStart())){
+                b=false;
+            }
+            if (ev.getStart().isAfter(e.getStart())){
+                if (e.getStart().plus(e.getDuration()).isAfter(ev.getStart()) ){
+                    b=false;
+                }}
+        }
+        return b;
+    }
+
+
 }
--- src/main/java/agenda/Event.java
+++ src/main/java/agenda/Event.java
@@ -1,71 +1,51 @@
 package agenda;
 
 import java.time.*;
+import java.time.chrono.ChronoLocalDate;
 
 public class Event {
-
-    /**
-     * The myTitle of this event
-     */
     private String myTitle;
-    
-    /**
-     * The starting time of the event
-     */
+
     private LocalDateTime myStart;
 
-    /**
-     * The durarion of the event 
-     */
+
     private Duration myDuration;
 
 
-    /**
-     * Constructs an event
-     *
-     * @param title the title of this event
-     * @param start the start time of this event
-     * @param duration the duration of this event
-     */
+
     public Event(String title, LocalDateTime start, Duration duration) {
         this.myTitle = title;
         this.myStart = start;
         this.myDuration = duration;
     }
 
-    /**
-     * Tests if an event occurs on a given day
-     *
-     * @param aDay the day to test
-     * @return true if the event occurs on that day, false otherwise
-     */
     public boolean isInDay(LocalDate aDay) {
         // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        if((aDay.getDayOfYear() == myStart.getDayOfYear() &&  aDay.getYear() == myStart.getYear())
+                || (aDay.getDayOfYear() == myStart.plus(myDuration).getDayOfYear() && aDay.getYear() == myStart.plus(myDuration).getYear())
+                || (aDay.isAfter(ChronoLocalDate.from(myStart)) && aDay.isBefore(ChronoLocalDate.from(myStart.plus(myDuration))))){
+            return true;
+        }
+        return false;
     }
-   
-    /**
-     * @return the myTitle
-     */
+
     public String getTitle() {
         return myTitle;
     }
 
-    /**
-     * @return the myStart
-     */
-    public LocalDateTime getStart() {
+
+    protected LocalDateTime getStart() {
         return myStart;
     }
 
 
-    /**
-     * @return the myDuration
-     */
-    public Duration getDuration() {
+
+    protected Duration getDuration() {
         return myDuration;
     }
 
-   
-    
+
+    public String toString(){
+        return myTitle;
+    }
 }
